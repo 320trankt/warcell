678437551b0f034665203a135ae1b8b1b7291cb8
320trankt <320trankt@gmail.com>
2026-03-27 17:02:14 -0700

    add prototype asset files, add basic enemy, fix viewport for testing

--- scenes/combat/enemies/base_enemy/base_enemy.gd
+++ scenes/combat/enemies/base_enemy/base_enemy.gd
@@ -1,18 +1,25 @@
-extends CharacterBody3D
+extends Node3D
 
-@onready var mesh = $MeshInstance3D
+var mesh: MeshInstance3D
+
+func _ready():
+    mesh = _find_mesh_instance(self)
+
+func _find_mesh_instance(node: Node) -> MeshInstance3D:
+    for child in node.get_children():
+        if child is MeshInstance3D:
+            return child
+        var result = _find_mesh_instance(child)
+        if result:
+            return result
+    return null
 
 func take_hit():
-	# 1. Create a "Tween" for the flash and shake
-	var tween = create_tween()
-	
-	# 2. Flash Red (adjusting the Albedo color of the mesh)
-	# This assumes you have a StandardMaterial3D on your mesh
-	var mat = mesh.get_active_material(0)
-	
-	# Animate the color to red and back
-	tween.tween_property(mat, "albedo_color", Color.RED, 0.1)
-	tween.parallel().tween_property(self, "position:z", position.z + 0.2, 0.05) # Small jolt back
-	
-	tween.tween_property(mat, "albedo_color", Color.WHITE, 0.1)
-	tween.parallel().tween_property(self, "position:z", position.z, 0.1) # Return to position
+    var tween = create_tween()
+    var mat = mesh.get_active_material(0)
+    
+    tween.tween_property(mat, "albedo_color", Color.RED, 0.1)
+    tween.parallel().tween_property(self, "position:z", position.z + 0.2, 0.05)
+    
+    tween.tween_property(mat, "albedo_color", Color.WHITE, 0.1)
+    tween.parallel().tween_property(self, "position:z", position.z, 0.1)
